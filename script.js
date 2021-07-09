b22583ed947db9f32e51f6c32e03c00409c43ce0
Zach Ickes <zachickes@hotmail.com>
2021-07-09 13:18:04 -0600

    created operate function, and calculator object

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const calculator = {
-    add = (x, y) => x + y,
-    subtract = (x, y) => x - y,
-    multiply = (x, y) => x * y,
-    divide = (x, y) => x / y,
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y,
 };
+function operate(x, y, operator) {
+let answer;
+switch(operator) {
+    case '+': answer = calculator.add(x, y);
+    break;
+    case '-': answer = calculator.subtract(x, y);
+    break;
+    case '*': answer = calculator.multiply(x, y);
+    break;
+    case '/': 
+    if(y == 0) {
+        alert('You goof! You can\'t divide by zero!');
+    }
+    else {
+        answer = calculator.divide(x, y);
+    }
+    break;
+    default: answer = 0;
+}
+return answer;
+}
