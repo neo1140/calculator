175d6ae528ecd449765f007da2c8fd2f4a64cbd9
Zach Ickes <zachickes@hotmail.com>
2021-07-09 12:53:55 -0600

    getting started on the project set up shell html and beginning work on javascript

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator
+Creating a simple calculator utilising all of the things I've learned through the course thusfar.
---
+++ script.js
@@ -0,0 +1,6 @@
+const calculator = {
+    add = (x, y) => x + y,
+    subtract = (x, y) => x - y,
+    multiply = (x, y) => x * y,
+    divide = (x, y) => x / y,
+};
