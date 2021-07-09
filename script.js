60bf8824ec0683bc6c0f66d4a4c6231941e2428c
Zach Ickes <zachickes@hotmail.com>
2021-07-09 15:13:32 -0600

    Added logic to the calculator buttons, also added some style. Need to add a display for user inputs, and for outputs.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+//Object holding all the mathmatical functions
 const calculator = {
     add: (x, y) => x + y,
     subtract: (x, y) => x - y,
     multiply: (x, y) => x * y,
     divide: (x, y) => x / y,
 };
+//Object holding the users input
+const inputs = {
+    valueOne: '',
+    valueTwo: '',
+    operator: '',
+};
+//Function that calls the proper calculator function using user inputs
 function operate(x, y, operator) {
 let answer;
 switch(operator) {
@@ -23,5 +31,32 @@ switch(operator) {
     break;
     default: answer = 0;
 }
-return answer;
+console.log(answer);
 }
+//Adding event listener to all the buttons except for the = button
+const buttons = document.querySelectorAll('button');    
+buttons.forEach((button) => {
+    if(isFinite(button.id)){
+    button.addEventListener('click', () => {
+        if(inputs.operator == ''){
+            inputs.valueOne += button.id; 
+        }
+        else
+            inputs.valueTwo += button.id;
+    });
+    }
+    else if(button.classList =='operator'){
+        button.addEventListener('click', () =>{
+            inputs.operator = button.id;
+        });
+    }
+});
+//Adding event listener to the = button to call the operate function
+const evaluate = document.getElementById('=');
+evaluate.removeEventListener('click', () =>{
+    inputs.operator = button.id;
+});
+evaluate.addEventListener('click', () =>{
+    operate(inputs.valueOne, inputs.valueTwo, inputs.operator);
+})
+
